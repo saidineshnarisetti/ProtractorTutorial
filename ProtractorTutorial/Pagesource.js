c05a5e6230d6a9bd4ec9076d9e5ec53e2345b07b
saidineshnarisetti <dinu.narisetti@gmail.com>
2019-05-18 16:46:08 +0530

    Commit

--- ProtractorTutorial/Pagesource.js
+++ ProtractorTutorial/Pagesource.js
@@ -2,11 +2,17 @@
 describe("Protractor Demo Page source", function() {
   it("Page source", function() {
 	  //browser.waitForAngularEnabled(false);  // for non- angular apps
-	// browser.get('http://google.com');  
+	// browser.get('https://apps-stage.juniper.net/newhome/');  
     browser.get('https://apps-stage.juniper.net/newhome/').then( function () {
          browser.getPageSource().then(function (txt) {
         console.log(txt);
       });
+         element(by.className("appmenu-wrapper")).element(by.className("app-full-name")).getText().then(function(text){
+         	console.log(text);
+         })
+         element.all(by.className("app-wrap")).getText().then(function(num){
+    	console.log(num);
+    })
     });
   });
 });
--- ProtractorTutorial/configuration.js
+++ ProtractorTutorial/configuration.js
@@ -1,6 +1,6 @@
 exports.config = {
 //  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['Chainlocators.js']
+  specs: ['Pagesource.js']
 //  multiCapabilities: [{
 //	  'browserName': 'firefox'
 //  }, {
